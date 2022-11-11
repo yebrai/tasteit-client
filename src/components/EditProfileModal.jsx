ac239b80aa9fced645f0f7bef2f9774e7c9c68de
yebrai <ivansevv@gmail.com>
2022-11-11 21:55:16 +0100

    structure modal editProfileModal done

--- src/components/EditProfileModal.jsx
+++ src/components/EditProfileModal.jsx
@@ -1,17 +1,76 @@
 import { Button, Modal } from "antd";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+
 
 function EditProfileModal() {
+    const navigate = useNavigate();
+    
 
-      const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [age, setAge] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+    // Modal configuration
+    const [open, setOpen] = useState(false);
+    const [confirmLoading, setConfirmLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSignup = async () => {
+        // const { authenticateUser } = useContext(AuthContext)
+    
+        try {
+          // Call backend route, create
+        //   await signupService(newUser);
+        //   const response = await loginService(userCredentials);
+        //   localStorage.setItem("authToken", response.data.authToken);
+          setConfirmLoading(true);
+          setTimeout(() => {
+            setOpen(false);
+            setConfirmLoading(false);
+            // authenticateUser();
+          }, 1000);
+    
+        } catch (error) {
+          if (error.response && error.response.status === 400) {
+            setErrorMessage(error.response.data.errorMessage);
+          } else {
+            // Error 500
+            navigate("/error");
+          }
+        }
+      };
+
+
+    // Modal functions
+    const showModal = () => {
+        setOpen(true);
+      };
+    
+      const handleOk = () => {
+        handleSignup();
+      };
+    
+      const handleCancel = () => {
+        setOpen(false);
+      };
+
   return (
-    <div>editProfileModal</div>
+    <>
+    <Button type="primary" onClick={showModal}>
+      Signup
+    </Button>
+    <Modal
+      title="User Login"
+      open={open}
+      onOk={handleOk}
+      confirmLoading={confirmLoading}
+      onCancel={handleCancel}
+    >
+      <div>
+        <form>
+ 
+        </form>
+      </div>
+    </Modal>
+  </>
   )
 }
 
