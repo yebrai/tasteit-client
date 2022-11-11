c75c94c1b4cc5422c7d344fbe8a4b2f6ca6a056f
yebrai <ivansevv@gmail.com>
2022-11-11 10:18:43 +0100

    added calls on home function for diferent api info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,43 @@
-import { Image } from 'antd';
+import axios from "axios"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import img from "../assets/all.jpg"
+import img2 from "../assets/bebidas.jpg"
+import img3 from "../assets/comidas.jpg"
+import img4 from "../assets/postre.jpg"
+
+
+
 
 function Home() {
+
+  const [ list, setList ] = useState([])
+  const [ isFetching, setIsFetching ] = useState(true)
+  const user = "user"
+  
+  const handleFood = async(type) => {
+    try {
+      const response = await axios.get(`http://localhost:5005/api/${type}`)
+      console.log(response)
+      setList(response.data)
+      setIsFetching(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  
+
   return (
-    <div style={{display: "flex", justifyContent:"center", alignItem:"center"}}>
-  <Image
-    width={150}
-    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-  />
-    <Image
-    width={150}
-    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-  />
-    <Image
-    width={150}
-    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-  />
-    <Image
-    width={150}
-    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-  />
+    <div >
+    <div className="homeBoxContainer">
+    <Link onClick={() => handleFood(user)} to="/products"><img src={img} alt="" width={400} height={300} /></Link>
+    <img onClick={() => handleFood()} src={img2} alt="" width={400} height={300} />
+    </div>
+    <div className="homeBoxContainer">
+    <img onClick={() => handleFood()} src={img3} alt="" width={400} height={300} />
+    <img onClick={() => handleFood()} src={img4} alt="" width={400} height={300} />
+    </div>
     </div>
   )
 }
