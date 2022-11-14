4e4150e0ecf9913cf1455089091b8e965df0508c
Borja Cabello Luna <borjacabello@outlook.es>
2022-11-14 15:54:25 +0100

    modified shopping cart

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -26,8 +26,6 @@ function AuthWrapper(props) {
     try {
       // 1 Verify token
       const response = await verifyService()
-      const shoppingCartCurrentProducts = await getShoppingCartService()
-      setCartProducts(shoppingCartCurrentProducts)
       setIsLoggedIn(true)
       // 2 Update user data
       setUser(response.data.user)
