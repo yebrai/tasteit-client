35a231bca7b0a87e0d98b23a7dd005e762fbbc58
Borja Cabello Luna <borjacabello@outlook.es>
2022-11-18 03:49:50 +0100

    added error 500 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import imgError from "../assets/error.png";
 
 function Error() {
   return (
-    <div>Error</div>
+    <div className="error-container">
+      <div className="error-wrapper">
+        <img src={imgError} alt="error-500" className="error-logo"/>
+        <Link className="main-buttons edit-button error-btn" to="/">Volver a TasteIt</Link>
+        <h2>El servidor se encuentra en tareas de mantenimiento. Disculpa las molestias.</h2>
+      </div>
+    </div>
   )
 }
 
